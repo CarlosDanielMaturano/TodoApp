da31fffb1dd438a67e524c958099a05bec3da1a2
CarlosDanielMaturano <carlosdanielmaturano@gmail.com>
2022-07-12 15:47:03 -0300

    modified:   index.html 	modified:   main.js

--- main.js
+++ main.js
@@ -12,16 +12,23 @@ function getCurrentTask(){
         }
         taskContent = resizedTask;
     }
-    return taskContent;
+    return [taskContent, "false"];
 }
 function createTask(taskContent = getCurrentTask()){
     if(taskContent == ""){
         alert("Don't leave field blanks!");
         return;
-    }  
+    }
+    let taskMarked =  "";
+    if(taskContent[1] == "true"){
+        taskMarked = "text-decoration: line-through";
+    }
+
+    taskText = `<p complete="${taskContent[1]}" style="${taskMarked}">${taskContent[0]}</p>`
     let currentTaskId = `task_${taskIdCount}`;
     let taskBody =`
-        <input type="checkbox" onclick="toggleDone('${currentTaskId}')"><p>${taskContent}</p>
+        <input type="checkbox" onclick="toggleDone('${currentTaskId}')">
+        ${taskText}
         <button onclick="removeTask('${currentTaskId}')">DELETE</button>
     `
     let task =  document.createElement("div");
@@ -29,7 +36,12 @@ function createTask(taskContent = getCurrentTask()){
     task.setAttribute('id', currentTaskId);
     task.innerHTML = taskBody;
     document.getElementById("tasks").appendChild(task);
+    if(taskContent[1] == "true"){
+        task.getElementsByTagName("input")[0].checked = true;
+    }
+    // console.log(taskContent[1]);
     taskIdCount++;
+    
 }
 function removeTask(taskId){
     document.getElementById("tasks").removeChild(
@@ -41,8 +53,36 @@ function toggleDone(id){
     let text = taskDiv.getElementsByTagName("p")[0];
     if(text.style.textDecoration == ""){
         text.style.textDecoration = "line-through";
+        text.setAttribute("complete", "true");
         return;
     }
     text.style.textDecoration = ""; 
-  
+    text.setAttribute("complete", "false");
 }
+
+function saveTasks(){
+    let documentTasks = document.getElementById("tasks").children;
+    let tasks = [];
+    for(let index = 0; index < documentTasks.length; index++){
+        taskContent = documentTasks[index].getElementsByTagName("p")[0];
+        tasks[index] = [taskContent.innerHTML, taskContent.getAttribute("complete")]
+        console.log(tasks[index]);
+    }
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function loadTasks(){
+    let tasks = JSON.parse(
+        localStorage.getItem("tasks")
+    )
+    for(let task of tasks){
+        createTask(task);
+    }
+}
+
+setInterval(saveTasks, 200);
+
+window.onload = function(){
+    loadTasks();
+}
+
